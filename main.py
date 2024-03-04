df834dd62f42224f202aaced502bb11d737d33c7
TahaThinks <tahaa4587@gmail.com>
2024-03-04 15:51:53 +0400

    Get the Row with the MAX TEMP

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import pandas
 
 data = pandas.read_csv("weather_data.csv")
-# print(data["temp"])
+# # print(data["temp"])
+#
+# data_dict = data.to_dict()
+# print(data_dict)
+#
+# temp_list = data["temp"].to_list()
+# print(temp_list)
+#
+# average_temp = data["temp"].mean()
+# max_temp = data["temp"].max()
 
-data_dict = data.to_dict()
-print(data_dict)
-
-temp_list = data["temp"].to_list()
-print(temp_list)
-
-average_temp = data["temp"].mean()
-max_temp = data["temp"].max()
+print(data[data.temp == data.temp.max()])
