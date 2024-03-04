05e25b6726f2535f8a1c3c54ded1565f188814ad
TahaThinks <tahaa4587@gmail.com>
2024-03-04 15:45:50 +0400

    Get Average & Max using the pandas Data Series

--- main.py
+++ main.py
@@ -9,5 +9,5 @@ print(data_dict)
 temp_list = data["temp"].to_list()
 print(temp_list)
 
-average_temp = sum(temp_list) / len(temp_list)
-print(round(average_temp,2))
+average_temp = data["temp"].mean()
+max_temp = data["temp"].max()
