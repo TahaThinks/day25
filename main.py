63e3dd2a41630996d33f97ba2218371f70fc7cf2
TahaThinks <tahaa4587@gmail.com>
2024-03-04 11:34:05 +0400

    Refine Code

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import csv
 
 with open("weather_data.csv") as data_file:
     data = csv.reader(data_file)
-    next(data)
     temperatues = []
     for row in data:
-        temperatues.append(int(row[1]))
+        if row[1] != "temp":
+            temperatues.append(int(row[1]))
 
     print(temperatues)
