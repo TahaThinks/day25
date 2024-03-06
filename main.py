5c48fe3bbd3a4eb139d2c67392a754b4ce28ee8c
TahaThinks <tahaa4587@gmail.com>
2024-03-06 12:53:35 +0400

    Create a Data Dictionary

--- main.py
+++ main.py
@@ -12,7 +12,13 @@ data = pandas.read_csv("weather_data.csv")
 # average_temp = data["temp"].mean()
 # max_temp = data["temp"].max()
 
-monday = data[data.day == "Monday"]
-monday_temp_c = monday.temp[0]
-monday_temp_f = (monday_temp_c * 9/5) + 35
-print(monday_temp_f)
+# monday = data[data.day == "Monday"]
+# monday_temp_c = monday.temp[0]
+# monday_temp_f = (monday_temp_c * 9/5) + 35
+# print(monday_temp_f)
+
+# Create a datafame from scratch
+data_dict = {
+    "students":["Amy", "James", "Angela"],
+    "scores":[76, 56, 65]
+}
