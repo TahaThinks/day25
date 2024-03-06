c42ce8a5a1c1570f13f293018107010641347992
TahaThinks <tahaa4587@gmail.com>
2024-03-06 13:24:11 +0400

    Squirrel Color Count Data

--- main.py
+++ main.py
@@ -18,10 +18,33 @@ import pandas
 # print(monday_temp_f)
 
 # Create a datafame from scratch
-data_dict = {
-    "students": ["Amy", "James", "Angela"],
-    "scores": [76, 56, 65]
+# data_dict = {
+#     "students": ["Amy", "James", "Angela"],
+#     "scores": [76, 56, 65]
+# }
+#
+# data = pandas.DataFrame(data_dict)
+# data.to_csv("new_data.csv")
+
+raw_data = pandas.read_csv("squirrel_raw_data.csv")
+squirrels_fur_color = raw_data["Primary Fur Color"]
+# print(squirrels_fur_color)
+cinnamon = 0
+black = 0
+gray = 0
+
+for squirrel_fur_color in squirrels_fur_color:
+    if squirrel_fur_color == "Gray":
+        gray += 1
+    elif squirrel_fur_color == "Cinnamon":
+        cinnamon += 1
+    elif squirrel_fur_color == "Black":
+        black += 1
+
+color_dict = {
+    "Fur Color":["grey", "red", "black"],
+    "Count": [gray, cinnamon, black]
 }
 
-data = pandas.DataFrame(data_dict)
-data.to_csv("new_data.csv")
+colors_count = pandas.DataFrame(color_dict)
+colors_count.to_csv("squirrel_count.csv")
