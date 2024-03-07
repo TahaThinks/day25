061512a4c4cf5d7e2564418f45a75af1b7a40eef
TahaThinks <tahaa4587@gmail.com>
2024-03-07 13:12:27 +0400

    Create a Pop up Window

--- main2.py
+++ main2.py
@@ -2,10 +2,11 @@ import turtle
 
 screen = turtle.Screen()
 screen.title("Taha U.S. Game")
-
 image = "blank_states_img.gif"
 screen.addshape(image)
-
 turtle.shape(image)
 
-screen.exitonclick()
+answer_state = screen.textinput(title="Guess the State", prompt="Enter a State:")
+print(answer_state)
+
+turtle.mainloop()
