d01292caf36f3290da656a7f00cec7b6b2786ce0
TahaThinks <tahaa4587@gmail.com>
2024-03-07 13:47:46 +0400

    Final Version

--- main2.py
+++ main2.py
@@ -2,11 +2,10 @@ import turtle
 import pandas
 
 us_states = pandas.read_csv("50_states.csv")
-print(us_states)
+# print(us_states)
 
 us_states_list = us_states.state.tolist()
-print(us_states_list)
-
+# print(us_states_list)
 
 screen = turtle.Screen()
 screen.title("Taha U.S. Game")
@@ -14,10 +13,22 @@ image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
 
-while True:
-    answer_state = screen.textinput(title="Guess the State", prompt="Enter a State:")
-    if answer_state.lower() in us_states_list:
+game_is_on = True
+
+while game_is_on:
+    answer_state = screen.textinput(title="Guess the State", prompt="Enter a State:").title()
+    if answer_state in us_states_list:
         print("Exists")
+        desired_row = us_states[us_states["state"] == answer_state]
+        x_coordinate = desired_row.x.item()
+        y_coordinate = desired_row.y.item()
+
+        turtle_state = turtle.Turtle()
+        turtle_state.hideturtle()
+        turtle_state.penup()
+
+        turtle_state.goto(x_coordinate,y_coordinate)
+        turtle_state.write(answer_state)
     else:
         print("Doesn't Exists")
 
