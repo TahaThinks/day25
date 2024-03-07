4356dc72e131305daf7ced9a54543c5341945c82
TahaThinks <tahaa4587@gmail.com>
2024-03-07 13:19:15 +0400

    Extract Data from csv using Pandas

--- main2.py
+++ main2.py
@@ -1,4 +1,12 @@
 import turtle
+import pandas
+
+us_states = pandas.read_csv("50_states.csv")
+print(us_states)
+
+us_states_list = us_states.state.tolist()
+print(us_states_list)
+
 
 screen = turtle.Screen()
 screen.title("Taha U.S. Game")
@@ -6,7 +14,12 @@ image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
 
-answer_state = screen.textinput(title="Guess the State", prompt="Enter a State:")
-print(answer_state)
+while True:
+    answer_state = screen.textinput(title="Guess the State", prompt="Enter a State:")
+    if answer_state.lower() in us_states_list:
+        print("Exists")
+    else:
+        print("Doesn't Exists")
+
 
 turtle.mainloop()
